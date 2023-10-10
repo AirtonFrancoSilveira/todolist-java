1b69c95ba951732dcc4c779c1ee0f01f54459f77
Airton Franco <airton.franco@rvmdigital.com.br>
2023-10-10 14:19:24 -0300

    update

--- src/main/java/br/com/rvmdigital/todolist/users/UserController.java
+++ src/main/java/br/com/rvmdigital/todolist/users/UserController.java
@@ -1,5 +1,6 @@
 package br.com.rvmdigital.todolist.users;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
     @PostMapping("/")
     public void UserModel(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+        this.userRepository.save(userModel);
     }
     
 }
